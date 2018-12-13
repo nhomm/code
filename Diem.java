0e0332d22ba53bde8a1d96c3383af9c1fce8d58d
nhomm <45851673+nhomm@users.noreply.github.com>
2018-12-13 22:40:06 +0700

    Add files via upload

---
+++ Diem.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author DELL-PC
+ */
+public class Diem {
+ private String MaMH;
+    private String TenMH;
+    private int SoTiet;
+    private int HocKy;
+    private String MaLop;
+    private String TenLop;
+    private int NamHoc;
+    private String MaHS;
+    private String TenHS;
+    private int Diem;
+    
+    
+    //Tên HS, Tên lớp, Tên môn học, Điểm, Số tiết, Học kỳ
+
+    public String getMaMH() {
+        return MaMH;
+    }
+
+    public void setMaMH(String MaMH) {
+        this.MaMH = MaMH;
+    }
+
+    public String getTenMH() {
+        return TenMH;
+    }
+
+    public void setTenMH(String TenMH) {
+        this.TenMH = TenMH;
+    }
+
+    public int getSoTiet() {
+        return SoTiet;
+    }
+
+    public void setSoTiet(int SoTiet) {
+        this.SoTiet = SoTiet;
+    }
+
+    public int getHocKy() {
+        return HocKy;
+    }
+
+    public void setHocKy(int HocKy) {
+        this.HocKy = HocKy;
+    }
+
+    public String getMaLop() {
+        return MaLop;
+    }
+
+    public void setMaLop(String MaLop) {
+        this.MaLop = MaLop;
+    }
+
+    public String getTenLop() {
+        return TenLop;
+    }
+
+    public void setTenLop(String TenLop) {
+        this.TenLop = TenLop;
+    }
+
+    public int getNamHoc() {
+        return NamHoc;
+    }
+
+    public void setNamHoc(int NamHoc) {
+        this.NamHoc = NamHoc;
+    }
+
+    public String getMaHS() {
+        return MaHS;
+    }
+
+    public void setMaHS(String MaHS) {
+        this.MaHS = MaHS;
+    }
+
+    public String getTenHS() {
+        return TenHS;
+    }
+
+    public void setTenHS(String TenHS) {
+        this.TenHS = TenHS;
+    }
+
+    public int getDiem() {
+        return Diem;
+    }
+
+    public void setDiem(int Diem) {
+        this.Diem = Diem;
+    }
+
+}
